893e4738ba92582843a131c6ce7d500ebe0e069a
agabhin <agabhin@node-1.cs739-abhinav.uwmadison744-f21-pg0.wisc.cloudlab.us>
2022-03-07 16:35:03 -0600

    workload random dir

--- scripts/workload.sh
+++ scripts/workload.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-
+rv=$RANDOM
 echo "create 2000 files of 4kb x 4"
 function c1000_4() {
     mkdir 1000_of_4kb
     for i in `seq 1 2000`; do
-        head -c 4K /dev/urandom >> 1000_of_4kb/$i
-        head -c 4K /dev/urandom >> 1000_of_4kb/$i
-        head -c 4K /dev/urandom >> 1000_of_4kb/$i
-        head -c 4K /dev/urandom >> 1000_of_4kb/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb_${rv}/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb_${rv}/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb_${rv}/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb_${rv}/$i
     done
 }
 time -p c1000_4
 
 echo "read 2000 files of 16kb"
-time -p cat 1000_of_4kb/* > /dev/null
+time -p cat 1000_of_4kb_${rv}/* > /dev/null
 
-rm -rf 1000_of_4kb
+rm -rf 1000_of_4kb_${rv}
 
 echo "mkdir: "
-time -p mkdir -p {a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i}/
+time -p mkdir -p ${rv}/{a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i}/
 
 
 echo "scandir: "
-time -p find . -type d | wc -l
+time -p find ${rv} -type d | wc -l
 
 
 echo "rmdir: "
-time -p rm -rf {a,b,c,d,e,f}/
+time -p rm -rf ${rv}
 
 
