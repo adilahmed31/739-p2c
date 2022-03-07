ca7e6b5ddf86e18c6de1b8b0729f87f7260d0506
agabhin <agabhin@node-1.cs739-abhinav.uwmadison744-f21-pg0.wisc.cloudlab.us>
2022-03-07 07:47:49 -0600

    create more dirs

--- scripts/workload.sh
+++ scripts/workload.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+
+echo "create 2000 files of 4kb x 4"
+function c1000_4() {
+    mkdir 1000_of_4kb
+    for i in `seq 1 2000`; do
+        head -c 4K /dev/urandom >> 1000_of_4kb/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb/$i
+        head -c 4K /dev/urandom >> 1000_of_4kb/$i
+    done
+}
+time -p c1000_4
+
+echo "read 2000 files of 16kb"
+time -p cat 1000_of_4kb/* > /dev/null
+
+rm -rf 1000_of_4kb
+
 echo "mkdir: "
-time mkdir -p {a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i}/
+time -p mkdir -p {a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i,j,k}/{a,b,c,d,e,f}/{g,h,i}/
 
 
 echo "scandir: "
-time find . -type d | wc -l
+time -p find . -type d | wc -l
 
 
 echo "rmdir: "
-time rm -rf {a,b,c,d,e,f}/
+time -p rm -rf {a,b,c,d,e,f}/
 
 
-echo "create 1000 files of 4kb"
-function c1000_4() {
-    mkdir 1000_of_4kb
-    for i in `seq 1 1000` do;
-        head -c 4K /dev/urandom > 1000_of_4kb/$i
-    done
-}
-time c1000_4
-
-echo "read 1000 files of 4kb"
-time cat 1000_of_4kb/* > /dev/null
-
-rm -rf 1000_of_4kb
